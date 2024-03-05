8825c09ad83c27de20d884f359a43b50abf35fd7
강경원 <73323028+kkwondev@users.noreply.github.com>
2024-03-05 08:22:01 +0000

    Quasar UI Framework 설치

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  typescript: { shim: false, typeCheck: true },
+  typescript: { shim: false },
+  modules: ['nuxt-quasar-ui'],
+  quasar: {
+    /* */
+  },
 });
